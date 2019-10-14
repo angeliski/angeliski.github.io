096f46aefe827af35d4165938fe4d5daf547613a
Rogerio Angeliski <angeliski@hotmail.com>
2019-10-14 12:29:19 -0300

    Formatando post

---
+++ .vuepress/theme/global-components/ImagePoster.vue
@@ -0,0 +1,24 @@
+<template>
+  <figure  style="text-align: center">
+  <img :src="src" :width="width">
+  <figcaption v-if="caption">{{caption}}</figcaption>
+</figure>
+</template>
+
+<script>
+//TODO procurar outro meio de lidar com o resolver de imagem
+export default {
+  props: {
+    caption: {
+      type: String,
+      default: ''
+    },
+    src: {
+      required: true
+    },
+    width: {
+      default: '320'
+    }
+  }
+}
+</script>
