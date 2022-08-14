0c62fa1344672f77cf46a529878efe1a31bcb8ce
Rogerio Angeliski <angeliski@hotmail.com>
2022-08-13 23:48:39 -0300

    add redirect rule (#84)

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: "slug",
-      value: `${slug.slice(0,12).replace(/-/g, '/')}${slug.slice(12)}`,
+      value: slug,
     })
   }
 }
